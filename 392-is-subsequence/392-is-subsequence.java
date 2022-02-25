dec860a024f5e5c7a4d20ecfdac279646ac40c64
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-02-25 22:55:18 +0530

    Time: 7 ms (9.17%), Space: 43.7 MB (5.04%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String X, String Y) {
+     return longestsubseq(X, Y,X.length(),Y.length());
+    }
+    public  boolean longestsubseq(String text1 ,String text2, int n ,int m)
+    {
+        
+        int dp[][]= new int[n+1][m+1];
+        
+        for(int i =1;i<n+1;i++)
+        {
+            for(int j =1;j<m+1;j++)
+            {
+                if(text1.charAt(i-1) == text2.charAt(j-1))
+                {
+                   dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+       if(dp[n][m] == text1.length())
+           return true;
+        else
+            return false;
+    }
+}
