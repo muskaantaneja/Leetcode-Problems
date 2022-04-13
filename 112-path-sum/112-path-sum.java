e59a243f62853813c8245f061a825b9916eaaf2b
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-04-13 12:39:40 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (91.50%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -17,25 +17,15 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int target) {
          if(root==null)
             return false;
-        return pathsum(root,target-root.val);
+        return pathsum(root,target);
     }
     public boolean pathsum(TreeNode root, int target)
     {
+        if(target-root.val==0 && root.right==null&&root.left==null)
+            return true;
         if(root==null)
             return false;
-        if(target==0 && root.right==null && root.left==null)
-        {
-        // System.out.print("yes");
-            return true;
-        }
-        else if(root.right==null && root.left==null)
-            return false;
-        if(root.left==null&&root.right!=null)
-        {
-            return pathsum(root.right, target-root.right.val);
-        }
-        else if(root.right==null&&root.left!=null)
-            return pathsum(root.left, target-root.left.val);
-        return pathsum(root.left, target-root.left.val)||pathsum(root.right, target-root.right.val);
+        
+        return hasPathSum(root.left, target-root.val)||hasPathSum(root.right, target-root.val);
     }
 }
