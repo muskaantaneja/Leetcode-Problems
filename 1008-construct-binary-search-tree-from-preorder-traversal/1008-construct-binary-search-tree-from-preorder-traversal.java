f8e52316aa9efcd19976cc95bfbd6c6dcbec1add
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-04-18 13:55:00 +0530

    Time: 2 ms (16.19%), Space: 40.5 MB (82.23%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -15,24 +15,31 @@
  */
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-    int[] preIndex = {0};
-        return buildTree(preorder, preIndex, 1001);
+        int[] inorder = new int[preorder.length];
+        for(int i =0;i<inorder.length;i++)
+        inorder[i] = preorder[i];
+        Arrays.sort(inorder);
+   HashMap<Integer, Integer> inorderHashMap = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            inorderHashMap.put(inorder[i], i);
+        }
+        int arr[] = new int[1];
+        arr[0] = 0;
+        return construct(preorder,arr,inorderHashMap,0,inorder.length-1);
     }
     
-    private TreeNode buildTree(int[] preorder, int[] preIndex, int boundVal){
-        
-        if(preIndex[0] >= preorder.length || preorder[preIndex[0]] >= boundVal){
+    private TreeNode construct(int[] preorder,int[] arr,HashMap <Integer,Integer> inorderHashMap,int l,int r)
+    {
+        if(l>r)
             return null;
-        }
         
-        TreeNode root = new TreeNode(preorder[preIndex[0]]);
-        preIndex[0] += 1;
-        
-        root.left = buildTree(preorder, preIndex, root.val);
-        root.right = buildTree(preorder, preIndex, boundVal);
+        TreeNode root = new TreeNode(preorder[arr[0]]);
+        int index = inorderHashMap.get(preorder[arr[0]]);
+        arr[0] = arr[0] +1;
+        root.left = construct(preorder,arr,inorderHashMap,l,index-1);
+        root.right = construct(preorder,arr,inorderHashMap,index+1,r);
         
         return root;
-        
     }
-    
 }
+   
