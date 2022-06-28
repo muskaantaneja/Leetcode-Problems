9b796f4fa02551b9f53cfe53f7062b4612928aca
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-06-28 13:25:23 +0530

    Time: 3 ms (75.51%), Space: 48.7 MB (7.27%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,43 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int c=0;
-        int sum=0;
-        ListNode dummynode=new ListNode(-1);
-        ListNode head=dummynode;
-        while(l1!=null&&l2!=null)
-        {
-            sum=l1.val+l2.val+c;
-             ListNode d=new ListNode(sum%10);
-           head.next=d;
-            c=sum/10;
-            l1=l1.next;
-            l2=l2.next;
-            head=head.next;
-        }
-        while(l1!=null)
-        {
-           sum=l1.val+c;
-           ListNode d=new ListNode(sum%10);
-           head.next=d;
-            c=sum/10;
-            l1=l1.next;
-            head=head.next; 
-        }
-        while(l2!=null)
-        {
-           sum=l2.val+c;
-           ListNode d=new ListNode(sum%10);
-           head.next=d;
-            c=sum/10;
-            l2=l2.next;
-            head=head.next; 
+        int carry = 0;
+        ListNode list = null;
+        ListNode head = null;
+        
+        while(l1!=null || l2!=null){
             
+            int sum = carry;
+            sum += (l1==null) ? 0 : l1.val;
+            sum += (l2 == null) ? 0 : l2.val;
+            
+            if(list == null){
+                list = new ListNode(sum % 10);
+                head = list;
+            }
+            else
+                list.next = new ListNode(sum % 10);
+            
+            if(list!= null && list.next != null)
+               list = list.next; 
+            carry =  sum/10;
+            if(l1 != null)
+            l1 = l1.next;
+            if(l2!=null)
+            l2 = l2.next;
         }
-        if(c!=0)
-        {
-            ListNode d=new ListNode(c);
-           head.next=d;
-        } 
-       return dummynode.next; 
+    
+    
+    if(carry != 0){
+        if(list == null){
+                list = new ListNode(carry);
+            head = list;
+            }
+            else
+                list.next = new ListNode(carry);
     }
+    
+    return head;
+}
 }
