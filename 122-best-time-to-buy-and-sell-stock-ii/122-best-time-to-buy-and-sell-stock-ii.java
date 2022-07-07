de62558068857870c399dcd948f94118e975d4e9
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-07-07 18:47:59 +0530

    Time: 95 ms (5.61%), Space: 58.8 MB (5.11%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+     return Math.max(0,buyandsell(prices , 0 , 1 , new HashMap<>()));
+    }
+    public int buyandsell(int[] prices , int current , int canbuy ,HashMap<String , Integer>memo){
+        
+       if(current >= prices.length )
+            return 0;
+        
+        String key = current + "_" + canbuy ;
+        
+        if(memo.containsKey(key))
+            return memo.get(key);
+        
+        int buy = 0,sell = 0,nothing=0;
+        
+        if(canbuy == 1)
+        {
+             buy =  -prices[current] + buyandsell(prices , current + 1 , 0 , memo);
+        }
+        else if( canbuy == 0){
+            
+            sell = prices[current] + buyandsell(prices , current + 1 , 1 ,  memo);
+        }
+         
+        nothing = buyandsell(prices , current + 1 , canbuy , memo);
+        
+        memo.put(key , Math.max(buy + sell , nothing));
+        return memo.get(key);
+    }
+}
