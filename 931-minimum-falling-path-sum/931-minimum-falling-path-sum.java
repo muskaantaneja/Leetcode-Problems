3f8062f4f4ac558315243915895bf5e2f0a6ac4d
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-07-12 17:37:50 +0530

    Time: 18 ms (6.29%), Space: 47.8 MB (31.87%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,35 +1,38 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int cost = Integer.MAX_VALUE;
-        int[][] memo = new int[n][n];
-        for(int i=0;i<matrix[0].length;i++){
-            cost = Math.min(cost, minCost(0, i, matrix, n, memo));
-        }
-        return cost;
-    }
-    public int minCost(int currentRow, int currentCol, int[][] matrix, int n, int[][] memo){
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
         
-        if(currentRow < 0 || currentRow >= n || currentCol < 0 || currentCol >= n )
-        return 10000;
+        for(int arr[] : dp)
+            Arrays.fill(arr , 200000);
         
-        if(currentRow==n-1){
-            return matrix[currentRow][currentCol];
+        for(int col = 0; col < m ; col++)
+        {
+            dp[0][col] = grid[0][col];
         }
         
-        String currentKey = currentRow + "_" + currentCol;
-        if(memo[currentRow][currentCol]!=0){
-            return memo[currentRow][currentCol];
+        for(int row = 0 ; row < n ; row++ ){
+            
+            for(int col = 0; col < m ; col++ )
+            {
+                
+                if(row + 1 < n)
+                {
+                    dp[row + 1][col] = Math.min(dp[row][col] + grid[row+1][col],dp[row+1][col]);
+                    
+                    if(col - 1 >= 0)
+                        dp[row + 1][col- 1] = Math.min(dp[row][col] + grid[row+1][col -1],dp[row+1][col - 1 ]);
+                    
+                    if(col + 1 < m)
+                        dp[row + 1][col + 1] = Math.min(dp[row][col] + grid[row+1][col + 1],dp[row+1][col +1 ]);
+                }
+            }
         }
-        
-        int downMove = matrix[currentRow][currentCol] + minCost(currentRow + 1, currentCol, matrix, n, memo);
-        
-        int diagLeftMove = matrix[currentRow][currentCol] +minCost(currentRow + 1,currentCol - 1, matrix, n, memo);
-        
-        int diagRightMove = matrix[currentRow][currentCol] +minCost(currentRow + 1,currentCol + 1,matrix, n, memo);
-        
-        memo[currentRow][currentCol] =  Math.min(downMove, Math.min(diagLeftMove, diagRightMove));
-        
-        return memo[currentRow][currentCol];
-    }
+        int ans = 100000;
+        for(int i = 0 ; i < n ; i++){
+            ans = Math.min(ans , dp[n-1][i]);
+        }
+        return ans;
 }
+    }
