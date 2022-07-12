43497a7a64ddf7f8f76d178a0bd8a964910f8625
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-07-12 14:35:03 +0530

    Time: 0 ms (100.00%), Space: 40 MB (99.75%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,27 +1,27 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-     return countpaths(obstacleGrid , obstacleGrid.length ,obstacleGrid[0].length , 0 , 0, new HashMap<>());
-    }
-    public int countpaths(int[][] grid ,int m , int n , int row , int col , HashMap<String,Integer> memo){
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
         
-        if(row == m-1 && col == n-1 && grid[row][col]!=1)
-            return 1;
-        
-        if( row >= m || col >= n || row < 0 || col<0 || grid[row][col] == 1){
-            
+        if(obstacleGrid[m-1][n-1] == 1)
             return 0;
+        
+        int dp[][]= new int[m][n];
+        
+        dp[m-1][n-1] = 1;
+        
+        for(int i = m-1 ; i >= 0 ; i--){
+            
+            for(int j = n-1 ; j >= 0 ; j--){
+                
+                if(obstacleGrid[i][j] != 1)
+                if(i-1 >= 0 && obstacleGrid[i-1][j] != 1)
+                    dp[i-1][j] += dp[i][j];
+                if(j-1 >= 0 && obstacleGrid[i][j -1] != 1)
+                    dp[i][j-1] += dp[i][j];
+            }
         }
         
-        String key = row + "_" + col;
-        
-        if(memo.containsKey(key))
-            return memo.get(key);
-        
-        int right = countpaths(grid ,m , n , row , col + 1 , memo);
-        int down = countpaths(grid ,m , n , row + 1, col , memo);
-        
-        memo.put( key , right + down);
-        
-        return memo.get(key);
-    }
+        return dp[0][0];
+}
 }
