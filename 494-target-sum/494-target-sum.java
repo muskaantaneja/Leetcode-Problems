280961df40c6ffb4a258b99a1c359324b90751c7
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-07-14 16:10:36 +0530

    Time: 1583 ms (5.03%), Space: 72.6 MB (6.01%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,24 +1,56 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         
-        return targetsum(nums , 0 , target , new HashMap<>());
-    }
-    public int targetsum(int[] nums , int current , int target , HashMap<String,Integer> map){
+        int sum = 0;
+        target = Math.abs(target);
+        for(int i : nums)
+        {
+            sum += i;
+        }
         
-        if( current >= nums.length && target == 0)
+        if((target + sum)% 2 != 0)
+            return 0;
+        if(nums.length == 1 && target+sum == 0 &&sum!=0&& sum == -target)
             return 1;
-        if( current >= nums.length && target != 0)
+        if(target == 0 && sum == 0)
+            return (int)Math.pow(2 ,nums.length);
+        
+        target = (target+ sum)/2;
+        
+        Arrays.sort(nums);
+        return subsetsum(nums , target) ;
+    }
+   public int subsetsum( int[] nums ,int target){
+        
+        if(target == 0)
             return 0;
         
-        String key = current +"_" + target;
-        if(map.containsKey(key))
-            return map.get(key);
+        int n = nums.length;
+        int dp[][] = new int[n+1][target+1];
         
-        int pos = targetsum( nums , current+1 , target + nums[current] , map);
-        int neg = targetsum( nums , current+1 , target - nums[current] , map);
+        for(int i = 0 ; i < target+1 ; i++){
+            dp[n][i] = 0;
+        }
         
-        map.put(key , pos + neg);
+        for(int i = 0 ; i < n+1 ; i++){
+            dp[i][0] = 1;
+        }
         
-        return map.get(key);
+        for(int current = n-1 ; current >= 0 ; current--){
+            
+            for(int t = 1 ; t <= target ; t++){
+    dp[current][t] = dp[current + 1][t] +  ((t - nums[current] >= 0) ? dp[current+1][t - nums[current]] : 0 );
+                
+            }
+        }
+       
+       for(int current =0 ; current <= n ; current++){
+            
+            for(int t = 0 ; t <= target ; t++){
+        System.out.print(dp[current][t] + "  ");
+            }
+           System.out.println();
+       }
+        return dp[0][target];
     }
 }
