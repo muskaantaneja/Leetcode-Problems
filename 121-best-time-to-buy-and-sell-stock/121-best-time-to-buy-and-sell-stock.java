33276ff0b9dd1a1c00ac7c342b289b926aae8c35
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-07-19 11:35:15 +0530

    Time: 164 ms (5.66%), Space: 123.6 MB (5.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,33 +1,78 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return buyandsell(prices , 0 , 1 , 1 , new HashMap<>());
+        return buyandsell(prices , 0 , 1 , 1 );
     }
-    public int buyandsell(int[] prices , int current , int canbuy ,int trans,HashMap<String , Integer>memo){
+    public int buyandsell(int[] prices , int current , int canbuy ,int trans){
         
         if(trans == 0 && canbuy == 1 )
             return 0;
         
         if(current >= prices.length || trans<0)
             return 0;
-        String key = current + "_" + canbuy + "_" + trans;
+       
+       int dp[][][]= new int[prices.length + 1][canbuy + 1][trans + 1];
         
-        if(memo.containsKey(key))
-            return memo.get(key);
         
-        int buy = 0,sell = 0,nothing=0;
         
-        if(canbuy == 1)
-        {
-             buy =  -prices[current] + buyandsell(prices , current + 1 , 0 , trans - 1,memo);
-        }
-        else if( canbuy == 0 && trans == 0){
+        for(int i = prices.length-1 ; i>=0 ; i--){//current
             
-            sell = prices[current] + buyandsell(prices , current + 1 , 1 , trans - 1, memo);
-        }
+            for(int j = 1; j >=0 ; j--){//canbuy
+                
+                for(int k = 1 ; k  < 2 ; k++){//trans
+                    int buy = 0,sell = 0,nothing=0;
+                     if(j == 1)
+                     {
+                        buy =  -prices[i] + dp[ i + 1][0][k];
+                     }
+                     else {
+            
+                         sell = prices[i] + dp[i + 1][1][k-1];
+                      }
          
-        nothing = buyandsell(prices , current + 1 , canbuy , trans ,memo);
-        
-        memo.put(key , Math.max(buy + sell , nothing));
-        return memo.get(key);
+                    nothing = dp[i + 1][j][k];
+                    dp[i][j][k] = Math.max(buy+sell , nothing);
+                }
+            }
+        }
+//      for(int i = 0 ; i<=prices.length ; i++){//current
+            
+//             for(int j = 0 ; j < 2 ; j++){//canbuy
+                
+//                 for(int k = 0 ; k<2 ; k++){//trans
+//                     System.out.print(dp[i][j][k] + " ");
+//                 }
+//             }
+//          System.out.println();
+//         }
+        return dp[0][1][1];
     }
-}
+    
+//     public int buyandsell(int[] prices , int current , int canbuy ,int trans,HashMap<String , Integer>memo){
+        
+//         if(trans == 0 && canbuy == 1 )
+//             return 0;
+        
+//         if(current >= prices.length)
+//             return 0;
+//         String key = current + "_" + canbuy + "_" + trans;
+        
+//         if(memo.containsKey(key))
+//             return memo.get(key);
+        
+//         int buy = 0,sell = 0,nothing=0;
+        
+//         if(canbuy == 1)
+//         {
+//              buy =  -prices[current] + buyandsell(prices , current + 1 , 0 , trans - 1,memo);
+//         }
+//         else if( canbuy == 0 && trans == 0){
+            
+//             sell = prices[current] + buyandsell(prices , current + 1 , 1 , trans - 1, memo);
+//         }
+         
+//         nothing = buyandsell(prices , current + 1 , canbuy , trans ,memo);
+        
+//         memo.put(key , Math.max(buy + sell , nothing));
+//         return memo.get(key);
+//     }
+ }
