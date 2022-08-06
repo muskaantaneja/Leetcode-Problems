d3a7c11b87ccad8cc5c634d68ebeebd2fe1e746b
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-08-06 14:08:32 +0530

    Time: 3 ms (69.64%), Space: 76.2 MB (83.14%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,32 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int canbuy = 1;
-        int trans = 1;
-        int dp[][][]= new int[prices.length + 1][canbuy + 1][trans + 1];
+        if(prices.length < 2)
+            return 0;
         
+        int buy = prices[0];
+        int profit = 0;
         
-        
-        for(int i = prices.length-1 ; i>=0 ; i--){//current
+        for(int current = 1 ; current < prices.length ; current++){
             
-            for(int j = 0; j <=1 ; j++){//canbuy
-                
-                for(int k = 1 ; k  < 2 ; k++){//trans
-                    int buy = 0,sell = 0,nothing=0;
-                     if(j == 1)
-                     {
-                        buy =  -prices[i] + dp[ i + 1][0][k];
-                     }
-                     else {
-            
-                         sell = prices[i] + dp[i + 1][1][k-1];
-                      }
-         
-                    nothing = dp[i + 1][j][k];
-                    dp[i][j][k] = Math.max(buy+sell , nothing);
-                }
-            }
+            if(prices[current] < buy)
+                buy = prices[current];
+            profit = Math.max(profit , prices[current] - buy);
         }
-
-        return dp[0][1][1];
+       // System.out.println(buy + " " + sell);
+        return profit;
     }
 }
