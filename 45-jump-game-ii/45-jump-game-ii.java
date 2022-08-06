9076c284feafc57ced25441b878e1bc0b7980603
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-08-06 14:55:41 +0530

    Time: 1 ms (99.89%), Space: 43.1 MB (87.54%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,27 +1,26 @@
 class Solution {
     public int jump(int[] nums) {
-     return jumpgame( nums , 0 , new HashMap<>());
-    }
-    
-    public int jumpgame(int[] nums , int current , HashMap<Integer,Integer> memo){
         
-        if(current >= nums.length-1)
+        if(nums.length == 1)
             return 0;
         
-        if(nums[current] == 0)
-            return 100000;
-        
-        if(memo.containsKey(current))
-            return memo.get(current);
-        
-        int ans = 1000000;
-        for(int jumps = 1 ; jumps <= nums[current] ; jumps++){
-            
-            ans = Math.min(ans , 1 +  jumpgame(nums , current + jumps , memo)); 
+        int maxjump = 0;
+        int steps = 0;
+        int prevjump = 0;
+        for(int current = 0 ; current < nums.length ; current++)
+        {
+            if(prevjump < current)
+           {
+               steps++;
+               prevjump = maxjump;
+           }
+            if(prevjump >= nums.length-1)
+                return steps;
+           
+           maxjump = Math.max(maxjump, current+nums[current]); 
             
         }
         
-        memo.put(current , ans);
-        return ans;
+        return 0;
     }
 }
