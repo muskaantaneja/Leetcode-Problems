58bff68dc99794d9c9de3d2c34749ac6e9c65dce
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-08-21 10:27:17 +0530

    Time: 1143 ms (8.08%), Space: 181.7 MB (5.00%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,59 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
+        Set<List<Integer>> res  = new HashSet<>();
+        if(nums.length==0) return new ArrayList<>(res);
         Arrays.sort(nums);
-        HashSet<List<Integer>> memo = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        for(int i = 0 ; i < nums.length - 1 ; i++){
- 
-            List<Integer> list = new ArrayList<>();
-            list.add(nums[i]);
-            twoSum( nums , i + 1 ,-nums[i] , list , memo);
-            System.out.println(list);
-        }
-        
-        for(List<Integer> arr : memo)
-            ans.add(arr);
-        
-        return ans;
-    }
-    public void twoSum(int[] nums,int start, int target , List<Integer>list , HashSet<List<Integer>> memo) {
-        HashSet<Integer> map = new HashSet<>();
-        
-        for(int current=start ; current<nums.length ; current++){
-        
-            int key = nums[current];
-            if(map.contains(key))
-            {
-                if(key + key == target)
-                {
-                    
-                   list.add(key);
-                    list.add(key);
-                    memo.add(new ArrayList(list));
-                    list.remove(list.size()-1);
-                    list.remove(list.size()-1);
-                }
+        for(int i=0; i<nums.length-2;i++){
+            int j =i+1;
+           int  k = nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+                else if ( sum >0) k--;
+                else if (sum<0) j++;
             }
-            
-            map.add(key);
+
         }
-        HashSet<Integer> set = new HashSet<>(map);
-        for(int current : map)
-        {
-            set.remove(current);
-            if(set.contains(target - current)){
-                list.add(target-current);
-                list.add(current);
-                
-                memo.add(new ArrayList(list));
-                list.remove(list.size()-1);
-                    list.remove(list.size()-1);
-            }
-           // set.add(current);
-        }
-        return ;
+        return new ArrayList<>(res);
         
     }
 }
