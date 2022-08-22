87e8b5f537c1597d680770b854c9764daf71464a
muskaantaneja <59204693+muskaantaneja@users.noreply.github.com>
2022-08-22 07:30:28 +0530

    Time: 44 ms (22.98%), Space: 60.5 MB (24.52%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1)
+            return "1";
+        if(n==2)
+            return "11";
+        return makeString(3 ,n , "11");
+    }
+    public String makeString(int current , int n , String ans)
+    {
+        if(current-1 == n)
+            return ans;
+        
+        int count = 0;
+        int len = 1;
+        count = 1;
+        String str = "";
+        
+        while(len < ans.length())
+        {
+            while(len < ans.length() && ans.charAt(len-1) == ans.charAt(len))
+            {
+                len++;
+                count++;
+            }
+            str = str + count +"" + ans.charAt(len-1);
+            count = 1;
+            len++;
+        }
+        
+        if(ans.length() > 1 && ans.charAt(ans.length() - 1) != ans.charAt(ans.length()-2))
+            str = str + count + "" + ans.charAt(ans.length()-1);
+        
+        //System.out.println("ans " + str);
+       return makeString(current + 1 , n , str);
+        
+    }
+}
